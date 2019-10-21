bde1e7af38c7c0795c1a011fd6fac02a1f41fbd3
david-PS <45911942+Zeref0cult@users.noreply.github.com>
2019-10-21 23:32:46 +0200

    [dpujol04] Pag 56 chapter2

--- My Project Folder/main.py
+++ My Project Folder/main.py
@@ -1,3 +1,7 @@
+from datasets import load_housing_data
 from datasets import fetch_housing_data
+from sklearn.model_selection import train_test_split
 
-fetch_housing_data()
+
+housing = load_housing_data()
+train_set, test_set = train_test_split(housing, test_size=0.2,random_state=42)
